bad1a8eb0a1a478c4e20b8d7eacdd31f71e7cfa9
sinke100 <88254228+sinke100@users.noreply.github.com>
2023-06-21 22:30:42 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+import cv2 as cv 
+import numpy as np
+
+app = Flask(__name__)
+app.template_folder = '.'
+
+def tr(data):
+    array = np.array(data,np.uint8)
+    img = cv.imdecode(array, cv.IMREAD_UNCHANGED)
+    qr = cv.QRCodeDetector()
+    value = qr.detectAndDecode(img)[0]
+    if value: return value
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/translate', methods=['POST'])
+def translate():
+    file = request.files['image']
+    data = tr(list(file.read()))
+    if data: return data
+    else: return "Can't read",400
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
