f887fd9dce9dfb138e3bec25c27e13ab7ea8f447
sinke100 <88254228+sinke100@users.noreply.github.com>
2026-03-07 14:20:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 from flask import Flask, render_template, request
 import cv2 as cv 
 import numpy as np
+import PIL.Image as im
+from pyzbar.pyzbar import decode
+from io import BytesIO as b
 
 app = Flask(__name__)
 app.template_folder = '.'
 
+def trp(data):
+    data = bytes(data)
+    img = im.open(b(data))
+    qr = decode(img)[0]
+    return qr.data.decode()
+    
 def tr(data):
     array = np.array(data,np.uint8)
     img = cv.imdecode(array, cv.IMREAD_UNCHANGED)
     qr = cv.QRCodeDetector()
     value = qr.detectAndDecode(img)[0]
+    if value: return value
+    value = trp(data)
     if value: return value
 
 @app.route('/')
@@ -25,3 +36,4 @@ def translate():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
